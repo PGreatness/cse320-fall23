f85e0a7804de80b109ee3316847455245d42e69e
Ahnaf Hasan <ahnaf.hasan@stonybrook.edu>
2023-08-30 15:38:13 -0400

    finish working on arg validation

---
+++ hw1/include/compare.h
@@ -0,0 +1,10 @@
+#include "global.h"
+#include "debug.h"
+
+/**
+ * Compares the two strings given. Returns whether or not they are equal.
+ * @param str1 a string of any length
+ * @param str2 a string of any length
+ * @returns 0 if the strings are different, else 1
+*/
+int compareStrings(char* str1, char* str2);
--- hw1/src/validargs.c
+++ hw1/src/validargs.c
@@ -3,6 +3,8 @@
 #include "global.h"
 #include "debug.h"
 
+#include "compare.h"
+
 /**
  * @brief Validates command line arguments passed to the program.
  * @details This function will validate all the arguments passed to the
@@ -20,6 +22,51 @@
  */
 int validargs(int argc, char **argv)
 {
-    // TO BE IMPLEMENTED.
-    abort();
+    if (argc < 1) return -1; // should never happen
+    if (argc == 1) return 0; // only program called, return success for validation
+    
+    for (int i = 2; i <= argc; i++)
+    {
+        // help option is called
+        if (compareStrings(*(argv + i - 1), "-h"))
+        {
+            if (i != 2) return -1; // help option must be the first option
+            global_options = global_options | HELP_OPTION;
+            return 0; // validation success, help requested
+        }
+
+        // matrix mode requested
+        if (compareStrings(*(argv + i - 1), "-m"))
+        {
+            // already set newick mode
+            if (global_options != 0) return -1;
+            global_options = global_options | MATRIX_OPTION;
+            continue;
+        }
+
+        // newick mode requested
+        if (compareStrings(*(argv + i - 1), "-n"))
+        {
+            // already set matrix mode
+            if (global_options != 0) return -1;
+            global_options = global_options | NEWICK_OPTION;
+            continue;
+        }
+
+        if (compareStrings(*(argv + i - 1), "-o"))
+        {
+            // not in newick mode
+            // validation fails
+            if ((global_options & NEWICK_OPTION) != NEWICK_OPTION) return -1;
+            // no file name given
+            if (i == argc) return -1;
+            outlier_name = *(argv + i++);
+            continue;
+        }
+
+        // improper argument given, fail validation
+        return -1;
+
+    }
+    return 0; // all validation checks passed, return success
 }
