c41c80d672f60bc3a24fef22caadd6c75cdd3fca
Ahnaf Hasan <ahnaf.hasan@stonybrook.edu>
2023-09-16 06:36:53 -0400

    add more EXIT_SUCCESS cases to main.c

--- hw1/src/main.c
+++ hw1/src/main.c
@@ -11,10 +11,27 @@ int main(int argc, char **argv)
     if(global_options == HELP_OPTION)
         USAGE(*argv, EXIT_SUCCESS);
     int read_data = read_distance_data(stdin);
-    // create a file
-    build_taxonomy(stdout);
+
+    // no global_options set, output edge data
+    if (global_options == 0)
+    {
+        build_taxonomy(stdout);
+        return EXIT_SUCCESS;
+    }
+    // -m flag set, output matrix data
+    if (global_options == MATRIX_OPTION)
+    {
+        emit_distance_matrix(stdout);
+        return EXIT_SUCCESS;
+    }
+    // -n flag set, output newick data
+    if (global_options == NEWICK_OPTION)
+    {
+        emit_newick_format(stdout);
+        return EXIT_SUCCESS;
+    }
     // TO BE IMPLEMENTED
-    return EXIT_FAILURE; 
+    return EXIT_FAILURE;
 }
 
 /*
