ebd67befc7df5374a861484cba39291c6ab2ac89
Ahnaf Hasan <ahnaf.hasan@stonybrook.edu>
2023-09-27 14:07:47 -0400

    fix error.c functions

--- hw2/include/error.h
+++ hw2/include/error.h
@@ -4,8 +4,8 @@
 #include <stdarg.h>
 
 void fatal(char *fmt, ...);
-void error(char *fmt, char *a1, char *a2, char *a3, char *a4, char *a5, char *a6);
-void warning(char *fmt, char *a1, char *a2, char *a3, char *a4, char *a5, char *a6);
-void debug(char *fmt, char *a1, char *a2, char *a3, char *a4, char *a5, char *a6);
+void error(char *fmt, ...);
+void warning(char *fmt, ...);
+void debug(char *fmt, ...);
 
 #endif
