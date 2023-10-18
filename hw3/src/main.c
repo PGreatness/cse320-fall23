770ee4ed5a2359abc2992f1c1b133fdaa750fbc4
Ahnaf Hasan <ahnaf.hasan@stonybrook.edu>
2023-10-18 19:56:13 -0400

    restore main

--- hw3/src/main.c
+++ hw3/src/main.c
@@ -2,17 +2,13 @@
 #include "sfmm.h"
 
 int main(int argc, char const *argv[]) {
-    // double* ptr = sf_malloc(sizeof(double));
+    double* ptr = sf_malloc(sizeof(double));
 
-	void *x = sf_malloc(112);
-	void *y = sf_realloc(x, 50);
+    *ptr = 320320320e-320;
 
-    sf_free(y);
-    // *ptr = 320320320e-320;
+    printf("%f\n", *ptr);
 
-    // printf("%f\n", *ptr);
-
-    // sf_free(ptr);
+    sf_free(ptr);
 
     return EXIT_SUCCESS;
 }
